4641d343c4f3a170c274799d091877c4e7f970e4
Susanta Ray <susantaray786@gmail.com>
2024-09-15 04:07:01 +0530

    ...

--- routes/auth.product.js
+++ routes/auth.product.js
@@ -13,17 +13,18 @@ const router = express.Router();
 router.get("/", getAllProducts);
 
 router.get("/", async (req, res) => {
-  const category = req.query.category;
+  const { category } = req.query;
   try {
     let products;
     if (category) {
-      products = await Product.find({ category });
+      products = await Product.find({ category: category });
     } else {
       products = await Product.find();
     }
     res.status(200).json(products);
   } catch (error) {
-    res.status(500).json({ msg: "Error fetching products" });
+    console.error("Error fetching products:", error);
+    res.status(500).json({ message: "Server Error" });
   }
 });
 
