cbc73f2fbe882120da5986ba851ce6f8bad3f60a
Susanta Ray <susantaray786@gmail.com>
2024-12-02 17:16:26 +0530

    added a /health endpoint for ping services ✔

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 
+app.get("/health", (req, res) => {
+  res.status(200).send("Ok");
+});
+
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
