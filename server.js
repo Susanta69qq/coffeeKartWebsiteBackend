c157d4179c3d68e10d802082e71ee7fd1ab1bbb3
Susanta Ray <susantaray786@gmail.com>
2024-12-03 02:28:17 +0530

    ....

--- server.js
+++ server.js
@@ -9,15 +9,18 @@ import cors from "cors";
 dotenv.config();
 connectDB();
 
+//setup
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//routes
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 
+//route for ping service and health checkup of backend service
 app.get("/health", (req, res) => {
   res.status(200).send("Ok working");
 });
